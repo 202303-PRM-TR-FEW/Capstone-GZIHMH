9dcbd4aea0b7d0cfcfaf66fb2ef779c6f2e09406
Zeyna Baaj <zainabaaj@gmail.com>
2023-07-07 03:04:09 +0300

    feat: level added to search page

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -5,6 +5,7 @@ import SearchBar from '@/components/SearchBar'
 import TopSearches from '@/components/TopSearches'
 import React from 'react'
 import categories_db from '@/utils/categories_db'
+import Star from '@/components/Star'
 const page = () => {
     const Top = ["Marketing Strategy", "UX Design", "Excel", "Adobe Photoshop", "CRM", "Photography", "Content Making"]
     const searchurl = "/assets/images/searchex.jpeg";
@@ -18,16 +19,16 @@ const page = () => {
             <h1>Find your favorites</h1>
 
             <SearchBar />
-            <hr className="my-2 border-gray-300 w-4/5 " />
+            <hr className="my-2 mt-8 border-gray-300 w-4/5 " />
             <h2>TOP SEARCHES</h2>
             <div className='flex flex-row'>
                 {Top.map((item, index) => (
-                    <TopSearches key={item} item={item} index={index} />
+                    <TopSearches item={item} index={index} />
                 ))}
             </div>
-            <hr className="my-2 border-gray-300 w-4/5 " />
+            <hr className="my-2 mt-8 border-gray-300 w-4/5 " />
             <h2>CATEGORIES</h2>
-            <div className='flex flex-row'>
+            <div className='flex flex-row my-5'>
                 {
                     categories_db.map((category) => (
                         <CheckElement id={category.id} name={category.name} />
@@ -35,8 +36,7 @@ const page = () => {
                     ))
                 }
             </div>
-            <hr className="my-2 border-gray-300 w-4/5 " />
-
+            <hr className="my-2 mt-8 border-gray-300 w-4/5 " />
             <div className='flex flex-row'>
                 <div className='flex flex-col mr-44'>
                     <h2>Rating</h2>
@@ -57,8 +57,9 @@ const page = () => {
                 </div>
             </div>
             <hr className="my-2 mt-8 border-gray-300 w-4/5 " />
+
             <h2>RECOMMENDED FOR YOU</h2>
-            <div className='flex flex-row'>
+            <div className='flex flex-row my-5'>
                 <Recommended thumbnail={searchurl} courseName={courseName} tutorName={tutorName} />
                 <Recommended thumbnail={searchurl} courseName={courseName} tutorName={tutorName} />
 
