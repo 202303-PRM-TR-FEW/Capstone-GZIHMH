8105342460cc155831831d40bca2614970475840
Zeyna Baaj <zainabaaj@gmail.com>
2023-07-07 21:48:16 +0300

    fix: Deployment

--- components/ListElement.jsx
+++ components/ListElement.jsx
@@ -6,7 +6,7 @@ import db from '../utils/db';
 
 function ListElement() {
   const [selectedCourseIndex, setSelectedCourseIndex] = useState(null);
-  
+
   const handleCourseClick = (index) => {
     setSelectedCourseIndex(index);
   };
@@ -18,33 +18,46 @@ function ListElement() {
 
   return (
     <div className='flex flex row flex-1'>
-       <div className='rounded-2xl p-2 '>
-       <p className="flex flex-row justify-between p-5">
-         <h1>My Learning</h1>
-         <p className='flex flex-row justify-between'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 self-start text-gray-500 hover:text-gray-600 fill-current m-2"><path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" /></svg><span className='w-430 h-42 flex-grow-1 m-0 overflow-hidden text-lg leading-10 text-left tracking-normal whitespace-pre-line opacity-90 visible text-gray-600 font-rubik'>Statistics</span> </p>
+      <div className='rounded-2xl p-2 '>
+        <p className="flex flex-row justify-between p-5">
+          <h1>My Learning</h1>
+          <p className='flex flex-row justify-between'><svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6 self-start text-gray-500 hover:text-gray-600 fill-current m-2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"
+            />
+          </svg><span className='w-430 h-42 flex-grow-1 m-0 overflow-hidden text-lg leading-10 text-left tracking-normal whitespace-pre-line opacity-90 visible text-gray-600 font-rubik'>Statistics</span> </p>
         </p>
-      {db.courses.map((course) => (
-        <div className='course_card' key={course.id} onClick={() => handleCourseClick(course.id)}>
-          <div className='flex flex-row justify-between '>
-          <Image src={course.image}  width={200} height={200} alt={course.title} className='image' style={{ cursor: 'pointer' }} />
-         </div>
-          <div className='ml-3'>
-          <h2>{course.title}</h2>
-          <p className='mb-3'>{getPersonName(course.userId)}</p>
-          <input
-            type="range"
-            value={parseInt(course.completion_ratio)}
-            className="w-full  opacity-100 bg-white rounded-full "
-          />
-          <p> {course.completion_ratio} complete</p>
+        {db.courses.map((course) => (
+          <div className='course_card' key={course.id} onClick={() => handleCourseClick(course.id)}>
+            <div className='flex flex-row justify-between '>
+              <Image src={course.image} width={200} height={200} alt={course.title} className='image' style={{ cursor: 'pointer' }} />
+            </div>
+            <div className='ml-3'>
+              <h2>{course.title}</h2>
+              <p className='mb-3'>{getPersonName(course.userId)}</p>
+              <input
+                type="range"
+                value={parseInt(course.completion_ratio)}
+                className="w-full  opacity-100 bg-white rounded-full "
+              />
+              <p> {course.completion_ratio} complete</p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
       <div>
-      {selectedCourseIndex !== null && (
-        <Details courseIndex={selectedCourseIndex} />
-      )}
+        {selectedCourseIndex !== null && (
+          <Details courseIndex={selectedCourseIndex} />
+        )}
       </div>
     </div>
   );
