cdf8e22c443d3f4a3e3074538cb490051b200274
mohammedhayaly <mohammed.alhayali88@yahoo.com>
2023-07-11 02:44:19 +0300

    sign in input added

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Login from '@/components/Login'
 import Image from 'next/image'
 
 export default function Intro() {
@@ -7,40 +8,8 @@ export default function Intro() {
 
 
       <div className="flex flex-col items-center flex-start m-auto ">
-        <div className='flex justify-start'>
-          <div className="flex flex-row  items-center  ">
-            <div>
-              <Image src="/assets/icons/icon.png"
-                alt="icon image"
-                width={70}
-                height={40}
-              />
-            </div>
-
-            <div>
-              <p className='text-primary font-bold'> LearnU </p>
-            </div>
-          </div>
-
-
-        </div>
-
-
-        <div>
-
-          <div className='text-gray-800 font-bold py-1 text-3xl '>
-            <p> Discover passion </p>
-          </div>
-
-          <div className=' text-gray-600 py-5 '>
-            <p> find out what topics you find interesting, <br /> learn a new skill & connect with people that <br /> are passionate about similer topics.  </p>
-          </div>
-
-
-          <div className="blue_btn  font-bold   w-40  py-5 ">
-            <button className="   py-1"> GET STARTED </button>
-          </div>
-        </div>
+        <Login />
+        
       </div>
 
       <div className='bg-white  rounded-3xl  '>
@@ -51,6 +20,10 @@ export default function Intro() {
 
         />
       </div>
+
+
+      
+
     </main>
   )
 }
---
+++ components/Intro.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <div>
+      
+
+
+      <div className='flex justify-start'>
+          <div className="flex flex-row  items-center  ">
+            <div>
+              <Image src="/assets/icons/icon.png"
+                alt="icon image"
+                width={70}
+                height={40}
+              />
+            </div>
+
+            <div>
+              <p className='text-primary font-bold'> LearnU </p>
+            </div>
+          </div>
+
+
+        </div>
+
+
+        <div>
+
+          <div className='text-gray-800 font-bold py-1 text-3xl '>
+            <p> Discover passion </p>
+          </div>
+
+          <div className=' text-gray-600 py-5 '>
+            <p> find out what topics you find interesting, <br /> learn a new skill & connect with people that <br /> are passionate about similer topics.  </p>
+          </div>
+
+
+          <div className="blue_btn  font-bold   w-40  py-5 ">
+            <button className="   py-1"> GET STARTED </button>
+          </div>
+        </div>
+
+
+
+
+    </div>
+  )
+}
+
+export default Intro
