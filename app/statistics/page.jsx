c8cf5449bbbe4e53e5f99c3bba34c3b51f22c11a
hayderali1 <haideralmamori94@gmail.com>
2023-07-11 00:46:29 +0300

    Feat:the year comparison component of the statistics page added style:styled using tailwind todo:review and merge

--- app/statistics/page.jsx
+++ app/statistics/page.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import LastYearComparison from '@/components/LastYearComparison';
-import ThisWeek from '@/components/ThisWeek';
+
 
 const page = () => {
     return (
         <div>
         <LastYearComparison />
-        <ThisWeek />
         </div>
     )
 }
