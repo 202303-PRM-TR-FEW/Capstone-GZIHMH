9c7a35944f2bed2293cefcf565a157d8aa38d6b1
hayderali1 <haideralmamori94@gmail.com>
2023-07-11 01:03:06 +0300

    Commit message describing your changes

--- app/statistics/page.jsx
+++ app/statistics/page.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import LastYearComparison from '@/components/LastYearComparison';
-
+import ThisWeek from '@/components/ThisWeek';
 
 const page = () => {
     return (
         <div>
         <LastYearComparison />
+        <ThisWeek />
         </div>
     )
 }
