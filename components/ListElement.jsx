35d5616570015427ff148263978701403d6303c5
zeyna baaj <zainabaaj@gmail.com>
2023-07-10 17:06:11 +0300

    Update ListElement.jsx

--- components/ListElement.jsx
+++ components/ListElement.jsx
@@ -5,9 +5,9 @@ import Details from './Details';
 import db from '../utils/db';
 
 function ListElement() {
-  const [selectedCourseIndex, setSelectedCourseIndex] = useState(null);
+  const [selectedCourseIndex, setSelectedCourseIndex] = useState(0);
   const [randomCourses, setRandomCourses] = useState([]);
-  const [savedCourses, setSavedCourses] = useState([]);
+  
 
   
 
@@ -22,7 +22,6 @@ function ListElement() {
 
     getRandomCourses();
   }, []);
-
 
   const handleCourseClick = (index) => {
     setSelectedCourseIndex(index);
@@ -64,7 +63,7 @@ const handleToggle = (courseId) => {
         {randomCourses.map((course) => (
           <div className='flex flex-row rounded-2xl justify-evenly m-5 items-center aspect-w-1 aspect-h-1 flex-1  w-576 h-70 opacity-100 bg-white hover:bg-blue-200 bg-opacity-30 rounded-5 border hover:border-blue-500' key={course.id} onClick={() => handleCourseClick(course.id)}>
             <div className='flex flex-row justify-between aspect-w-1 aspect-h-1'>
-              <Image src={course.image} width={100} height={100} alt={course.title} className='image object-cover' style={{ cursor: 'pointer' }} />
+              <Image src={course.image} width={200} height={180} alt={course.title} className='image object-cover' style={{ cursor: 'pointer' }} />
             </div>
             <div className='ml-3'>
               <h2>{course.title}</h2>
@@ -87,39 +86,10 @@ const handleToggle = (courseId) => {
       />
     </svg>
   </span>
-  {course.saved ? 'Unsave' : 'Save'}
+ 
 </button>
               
-              <p className='mb-3'>{getPersonName(course.user_id)}</p>
-
-    <div className='flex flex row flex-1'>
-      <div className='rounded-2xl p-2 '>
-        <p className="flex flex-row justify-between p-5">
-          <h1>My Learning</h1>
-          <p className='flex flex-row justify-between'><svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6 self-start text-gray-500 hover:text-gray-600 fill-current m-2"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"
-            />
-          </svg><span className='w-430 h-42 flex-grow-1 m-0 overflow-hidden text-lg leading-10 text-left tracking-normal whitespace-pre-line opacity-90 visible text-gray-600 font-rubik'>Statistics</span> </p>
-        </p>
-        {db.courses.map((course) => (
-          <div className='course_card' key={course.id} onClick={() => handleCourseClick(course.id)}>
-            <div className='flex flex-row justify-between '>
-              <Image src={course.image} width={200} height={200} alt={course.title} className='image' style={{ cursor: 'pointer' }} />
-            </div>
-            <div className='ml-3'>
-              <h2>{course.title}</h2>
-              <p className='mb-3'>{getPersonName(course.user_id)}</p>
-
+              <p className='tutor_name mb-3'>{getPersonName(course.user_id)}</p>
               <input
                 type="range"
                 value={parseInt(course.completion_ratio)}
@@ -128,19 +98,17 @@ const handleToggle = (courseId) => {
               <p> {course.completion_ratio} complete</p>
             </div>
             
-
-
           </div>
         ))}
       </div>
       <div>
-        {selectedCourseIndex !== null && 
+        {selectedCourseIndex !== null && (
           <Details courseIndex={selectedCourseIndex} />
-        }
+        )}
       </div>
       
     </div>
-  
+  );
 }
 
-export default ListElement;
+export default ListElement;
