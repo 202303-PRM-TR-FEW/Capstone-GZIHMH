658bc8d9ef3ba806d05e0d9d135075220bf65cf2
haroon <71440783+aaron176@users.noreply.github.com>
2023-07-10 14:47:58 +0300

    home category added
    i add home category

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Nav from '@/components/Nav';
+import HomeCategory from '@/components/HomeCategory';
 
 const Page = () => {
     return (
         <div>
             {/* <Nav />  */}
-
+<HomeCategory/>
         </div>
     );
 };
