954bc1dbb78eec03f09a0f025dab51d17659a0a4
Gulperi Ozbulut <gulperiozblt@gmail.com>
2023-07-11 22:03:11 +0300

    Added Featured Courses Comp.

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import Nav from '@/components/Nav';
 import HomeCategory from '@/components/HomeCategory';
+import FeaturedCourses from '@/components/FeaturedCourses';
 
 const Page = () => {
     return (
+        <section>
+            <div>
+                <h2 className="p-2 font-bold">Featured Courses</h2>
+                <div className="flex flex-col md:flex-row space-x-0 md:space-x-8 space-y-12  md:space-y-0 justify-center items-center mt-10">
+                    <FeaturedCourses />
+                    <FeaturedCourses />
+                    <FeaturedCourses />
+                    <FeaturedCourses />
+                </div>
+            </div>
         <div>
-           <Nav />  
-<HomeCategory/>
         </div>
+        </section>
+
     );
 };
 
