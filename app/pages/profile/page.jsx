c49a554a78c1db1fe7ff71984a061baadbd3077a
zeyna baaj <zainabaaj@gmail.com>
2023-07-13 23:11:08 +0300

    Update page.jsx

--- app/pages/profile/page.jsx
+++ app/pages/profile/page.jsx
@@ -17,10 +17,12 @@ const ProfilePage = () => {
         courses={coursesCount}
         followers={followersCount}
         following={followingCount}
-      />*/}
+      />
+       <FinishedCourses />
+      */}
 
      
-      <FinishedCourses />
+     
 
       <Achievements />
       
