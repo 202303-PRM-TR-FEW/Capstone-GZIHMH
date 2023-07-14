e74f432e7ca3b3256694183b7f193333b345248d
Izdihar <ezdiharomerrrr@gmail.com>
2023-07-14 14:44:20 +0300

    Style: the style of saved page added

--- app/pages/courses/page.jsx
+++ app/pages/courses/page.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 const page = () => {
     return (
         <div>
-            <div className="flex flex-col text-black">
+            <div className="text-black">
             <ListElement/>
         </div>
            
