7d6a20dbf8e2ef6baac0d13933affe21276e7600
hayderali1 <haideralmamori94@gmail.com>
2023-07-17 14:04:48 +0300

    Feat:FriendsSuggestion component added Fix:the profile page overlab fixed

--- app/pages/profile/page.jsx
+++ app/pages/profile/page.jsx
@@ -2,24 +2,18 @@ import React from 'react';
 import InfoBar from '@/components/InfoBar';
 import Achievements from '@/components/Achievements';
 import Friends from '@/components/Friends';
+import FriendsSuggestion from '@/components/FriendsSuggestion';
 
 const ProfilePage = () => {
-  const coursesCount = 3; // Update the number of finished courses
-  const followersCount = 2;
-  const followingCount = 32;
-
   return (
     <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
       <div className="md:col-span-2">
         <div className="md:sticky top-0">
-          <InfoBar
-            courses={coursesCount}
-            followers={followersCount}
-            following={followingCount}
-          />
+          <InfoBar />
         </div>
       </div>
-      <div className="md:col-span-2">
+      <div className="md:col-span-2 flex flex-col">
+        <FriendsSuggestion />
         <Friends />
       </div>
       <div className="md:col-span-2 md:col-start-3">
