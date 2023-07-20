f2453ba41e57b7ac1b0bde268a4c0b02f4298b0b
mohammedhayaly <mohammed.alhayali88@yahoo.com>
2023-07-21 00:54:20 +0300

    my activity and my activity box done

---
+++ components/MyActivitiesBox.jsx
@@ -0,0 +1,27 @@
+
+import React from 'react'
+import Activities from './Activities'
+
+const MyActivitiesBox = () => {
+  return (
+    
+
+            
+
+    <div>  
+
+<div className="flex items-center justify-between mb-4">
+        <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Latest Customers</h5>
+      </div> 
+        
+
+      <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-3xl shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+
+            <Activities />
+
+      </div>
+    </div>
+  )
+}
+
+export default MyActivitiesBox
---
+++ utils/MyActivityData.js
@@ -0,0 +1,20 @@
+const MyActivityData = [
+    {
+      title: 'Becoming a photographer',
+      name: 'Clara Manning',
+      image: '/assets/images/profilePic5.jpeg',
+    },
+    {
+      title: 'Design Thinking 2.0',
+      name: 'Chris Kinley',
+      image: '/assets/images/2c1a496b-9948-4cb1-b87e-dbd417261382.jpg',
+    },
+    {
+      title: 'Becoming a photographer',
+      name: 'Clara Manning',
+      image: '/assets/images/profilePic5.jpeg',
+    },
+    // Add more friend profiles as needed
+  ];
+  
+  export default MyActivityData;
