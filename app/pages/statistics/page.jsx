77ce15e4e6b0e535721233cf91b7e138b73ff0c8
mohammedhayaly <mohammed.alhayali88@yahoo.com>
2023-07-24 18:17:01 +0300

    page structure added and one component modified

--- app/pages/statistics/page.jsx
+++ app/pages/statistics/page.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import LastYearComparison from "@/components/LastYearComparison";
+import LastYearComparisonContainer from "@/components/LastYearComparisonContainer";
 import StatisticsGoalStreak from "@/components/StatisticsGoalStreak";
 import Activities from "@/components/Activities";
 import MyActivitiesBox from "@/components/MyActivitiesBox";
-
 import ThisWeekContainer from "@/components/ThisWeekContainer";
 
 const page = () => {
@@ -11,15 +10,15 @@ const page = () => {
   
 
 
-<div className="flex flex-col sm:flex-row   justify-between   space-y-4 sm:space-y-0 sm:space-x-4  ">
+<div className="flex flex-col sm:flex-row items-start m-5 justify-between gap-20 p-10 space-y-4 sm:space-y-0 sm:space-x-4  ">
 
 
 
 
 {/* Left Column */}
-<div className="flex flex-col p-1 bg-gray-100 rounded-lg  mr-20 ">
+<div className="flex flex-col p-1 bg-gray-100 rounded-lg">
 
- 
+  
 
 
   <div className="m-1">  * {/* box 1 */}
@@ -27,28 +26,59 @@ const page = () => {
   </div>
   
 
+
+
+
+
+
   <div className="m-1">   {/* box 2 */}
   <StatisticsGoalStreak />
+  
   </div>
+
+
+  <div className="m-1">   {/* box 3 (My performance component will be included later) */}
+  
+  </div>
+
+  
+
 
  </div>
 
 
 
+
+
+
+
+
 {/* Right Column */}
-<div className="flex  flex-col py-10 space-y-4 bg-gray-100 rounded-lg ml-10 "> 
+<div className="flex flex-col  p-4 space-y-4 bg-gray-100 rounded-lg"> 
 
 
 
-<div className="m-1 ">   {/* box 3 */}
+
+<div className="m-1">   {/* box 4 */}
+
 <MyActivitiesBox />
+
 </div>
 
 
-<div className="">  {/* box 4 */}
+
+
+
+<div className=" ">  {/* box 5 */}
+
+<LastYearComparisonContainer />
+
 
 </div> 
   
+  
+
+
 
 
 </div>
