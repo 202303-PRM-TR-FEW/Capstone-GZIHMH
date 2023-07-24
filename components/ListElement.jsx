e180c8e794eb06f959252c9224297178e9a1c99f
Izdiharomer <127107528+Izdiharomer@users.noreply.github.com>
2023-07-24 18:41:44 +0300

    158 fix courses style (#169)
    * Style: the size of the component fixed
    
    * Update SavedCourses.jsx
    
    * Fix: icons were added to a separated component
    
    * Update ListElement.jsx
    
    ---------
    
    Co-authored-by: zeyna baaj <zainabaaj@gmail.com>

--- components/ListElement.jsx
+++ components/ListElement.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Details from './Details';
 import db from '../utils/db';
+import icons from '../utils/icons';
 
 function ListElement() {
   const [selectedCourseIndex, setSelectedCourseIndex] = useState(0);
@@ -50,14 +51,15 @@ function ListElement() {
 
 
   return (
+    <div className='flex flex-col lg:flex-row md:flex-col sm:flex-col ml-10 lg:ml-10 md:ml-5 sm:ml-1'>
 <div className='flex flex-col lg:flex-row md:flex-col sm:flex-col ml-10 lg:ml-10 md:ml-5 sm:ml-1'>
       <div className='rounded-2xl p-2 flex flex-col w-1/2'>
         <div className="flex flex-row justify-between mt-5 lg:mt-5 md:mt-2 sm:mt-1 h-full">
           <h1>My Learning</h1>
           <div className='flex flex-row justify-between  px-3 py-2 text-gray-400 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-primary hover:text-gray-700'>
             <a className="flex text-lg items-center" href="/pages/statistics">
-              <svg xmlns="http://www.w3.org/2000/svg" className="mr-4" width="33" height="33" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 3v18h18" /><path d="M20 18v3" /><path d="M16 16v5" /><path d="M12 13v8" /><path d="M8 16v5" /><path d="M3 11c6 0 5 -5 9 -5s3 5 9 5" /></svg>
-              <span>Statistics</span>
+              {icons.map(icon => icon.name === 'statistics' && (<div key={icon.id} dangerouslySetInnerHTML={{ __html: icon.svg }} />))}
+              <span className='px-3 py-2'>Statistics</span>
             </a>
           </div>
         </div>
@@ -70,8 +72,9 @@ function ListElement() {
               <div className='flex justify-between'>
                 <h2>{course.title}</h2>
                 <button onClick={() => handleToggle(course.id)}>
-                  <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-6 h-6 self-start ${course.saved ? 'text-primary' : 'text-gray-400'} fill-current m-2`}>
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" /></svg></span></button>
+                  <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-6 h-6 self-start ${course.saved ? 'text-primary' : 'text-gray-400'} fill-current m-2`}><path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" /></svg>
+                  </span></button>
               </div>
               <p className='tutor_name mb-3'>{getPersonName(course.user_id)}</p>
               <input
@@ -90,7 +93,7 @@ function ListElement() {
           <Details courseIndex={selectedCourseIndex} />
         )}
       </div>
-
+</div>
     </div>
   );
 }
