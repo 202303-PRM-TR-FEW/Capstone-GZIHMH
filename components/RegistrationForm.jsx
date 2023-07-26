1b63b73957e704b96d332fda6d249be7c7d285a7
Zeyna Baaj <zainabaaj@gmail.com>
2023-07-26 23:25:59 +0300

    last update

--- components/RegistrationForm.jsx
+++ components/RegistrationForm.jsx
@@ -13,23 +13,7 @@ const RegistrationForm = () => {
     }
   };
 
-  const handleUpload = () => {
-    if (!selectedImage) return;
-
-    // const uploadTask = storage.ref(`images/${selectedImage.name}`).put(selectedImage);
-
-    uploadTask.on(
-      'state_changed',
-      null,
-      (error) => {
-        console.error('Error uploading image:', error);
-      },
-      () => {
-        // Image upload is complete
-        console.log('Image uploaded successfully!');
-      }
-    );
-  };
+ 
   return (
     <div className=" w-full " action="#">
       <div className=" ml-8 mb-4 flex flex-row  items-center h-full justify-start ">
@@ -57,8 +41,8 @@ const RegistrationForm = () => {
                 
 
                   <div className='mb-4'>
-                      <label for="email" className="block mb-2 ml-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                      <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name" required="" />
+                      <label for="name" className="block mb-2 ml-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                      <input type="name" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name" required="" />
                   </div>
 
                           
