da7583d544fa5cd6624509247f0f0085f309b6de
Zeyna Baaj <zainabaaj@gmail.com>
2023-07-26 04:34:35 +0300

    signup button

--- components/RegistrationForm.jsx
+++ components/RegistrationForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 const RegistrationForm = () => {
   return (
     <div className=" w-full " action="#">
@@ -74,6 +75,12 @@ const RegistrationForm = () => {
                       <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                   </div>
                   
+                  
+                  <div>
+                    <Link href = '/pages/home'>
+                      <button className=" blue_btn  m-2 py-1"  > Sign Up </button>
+                    </Link>
+                  </div>
                
 
 
