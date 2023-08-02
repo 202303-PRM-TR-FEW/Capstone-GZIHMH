ca4c9ae746ffefcabe62ca8aa55bf4dd38257106
Izdihar <ezdiharomerrrr@gmail.com>
2023-08-02 15:07:09 +0300

    Feat: no-saved-courses-message-component added

---
+++ components/NoCourseMessage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+function NoCourseMessage() {
+    return (
+        <div className='absolute top-1/4 left-1/4  items-center justify-center text-center h-full  w-full max-w-[600px] max-h-[500px] '>
+            <div className='bg-white pt-20 rounded-2xl justify-center text-gray-900 font-bold shadow-lg h-full  w-full '>
+                <div className='text-base sm:text-base md:text-base lg:text-base xl:text-base 2xl:text-base @screen (max-width: 300px) text-xs'>
+                    <span>Oops!</span> 
+                    <br/>
+                 You do not have any saved any courses yet
+                  😢
+                </div>
+                <div className='flex sm:text-xs md:text-sm lg:text-3xl text-3xl flex-col items-center justify-center mt-10 lg:mt-10 md:mt-3 sm:mt-1 gap-2'>
+                  <a className="flex text-lg items-center" href="/pages/home"><button className="text-primary font-bold bg-transparent border border-primary p-2 max-w-[450px] w-full  text-sm h-9  rounded-2xl hover:text-white hover:bg-primary"> Check our featured courses </button> </a>
+                  <a className="flex text-lg items-center" href="/pages/search"><button className="text-primary font-bold bg-transparent border border-primary p-2 max-w-[450px] w-full  h-9 text-sm  rounded-2xl hover:text-white hover:bg-primary">Search your favorite courses</button> </a>
+            </div>
+                </div>
+           
+        </div>
+    );
+}
+
+export default NoCourseMessage;
--- components/SavedCourses.jsx
+++ components/SavedCourses.jsx
@@ -1,13 +1,17 @@
+
 'use client'
 import React, { useState } from 'react';
 import Image from 'next/image';
 import db from '../utils/db';
+import NoCourseMessage from './NoCourseMessage';
 import SavedCourseReview from './SavedCourseReview';
 import { useSelector } from 'react-redux';
 import { selectSavedCourses } from '@/redux/selectors';
+
 function SavedCourses() {
   const [selectedCourseIndex, setSelectedCourseIndex] = useState(0);
   const savedCourses = useSelector(selectSavedCourses);
+
   const handleCourseClick = (index) => {
     setSelectedCourseIndex(index);
   };
@@ -18,59 +22,68 @@ function SavedCourses() {
   };
 
   return (
-
     <div className='flex flex-col lg:flex-row md:flex-col sm:flex-col h-full w-full'>
-      <div className='w-full'>
-        <h1 className='w-full px-4 py-1'>Saved Courses</h1>
-        <div className='lg:max-h-[800px] h-full overflow-y-auto'>
-          {savedCourses.map((course, index) => (
-            <div className='flex rounded-2xl opacity-100 bg-white hover:bg-blue-200 bg-opacity-30 rounded-5 border hover:border-primary' key={course.id}>
-              <div className='flex'>
-                <Image
-                  src={course.image}
-                  width={180}
-                  height={148}
-                  alt={course.title}
-                  className='max-w-[180px] max-h-[148px]'
-                  style={{ cursor: 'pointer' }}
-                  onClick={() => handleCourseClick(index)}
-                />
-              </div>
+      {savedCourses.length === 0 ? (
+        <NoCourseMessage />
+      ) : (
+        <>
+          <div className='w-full'>
+            <h1 className='w-full px-4 py-1'>Saved Courses</h1>
+            <div className='lg:max-h-[800px] h-full overflow-y-auto'>
+              {savedCourses.map((course, index) => (
+                  <div
+                  className={`flex rounded-2xl opacity-100 bg-white ${
+                    selectedCourseIndex === index ? 'border-primary bg-blue-200' : 'hover:bg-blue-200'
+                  } bg-opacity-30 rounded-5 border hover:border-primary`}
+                  key={course.id}
+                >
+                  <div className='flex'>
+                    <Image
+                      src={course.image}
+                      width={180}
+                      height={148}
+                      alt={course.title}
+                      className='max-w-[180px] max-h-[148px]'
+                      style={{ cursor: 'pointer' }}
+                      onClick={() => handleCourseClick(index)}
+                    />
+                  </div>
 
-              <div className='w-full'>
-                <div className='flex justify-between pt-2 pr-2 '>
-
-                  <h2>{course.title}</h2>
-                  <span>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      fill='none'
-                      viewBox='0 0 24 24'
-                      strokeWidth={1.5}
-
-                      className='w-5 h-5 text-primary fill-current'
-
-                    >
-                      <path
-                        strokeLinecap='round'
-                        strokeLinejoin='round'
-                        d='M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z'
-                      />
-                    </svg>
-                  </span>
+                  <div className='w-full'>
+                    <div className='flex justify-between pt-2 pr-2 '>
+                      <h2>{course.title}</h2>
+                      <span>
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          fill='none'
+                          viewBox='0 0 24 24'
+                          strokeWidth={1.5}
+                          className='w-5 h-5 text-primary fill-current'
+                        >
+                          <path
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            d='M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z'
+                          />
+                        </svg>
+                      </span>
+                    </div>
+                    <p className='tutor_name'>{getPersonName(course.user_id)}</p>
+                    <button className='text-white bg-primary w-12 h-6 text-sm font-semibold rounded-2xl mt-4'>
+                      BUY
+                    </button>
+                  </div>
                 </div>
-                <p className='tutor_name'>{getPersonName(course.user_id)}</p>
-                <button className='text-white bg-primary w-12 h-6 text-sm font-semibold rounded-2xl mt-4'>BUY</button>
-              </div>
+              ))}
             </div>
-          ))}
-        </div>
-      </div>
-      <div className='w-full h-full bg-white'>
-        {selectedCourseIndex !== null && (
-          <SavedCourseReview course={savedCourses[selectedCourseIndex]} />
-        )}
-      </div>
+          </div>
+          <div className='w-full h-full bg-white'>
+            {selectedCourseIndex !== null && (
+              <SavedCourseReview course={savedCourses[selectedCourseIndex]} />
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 }
