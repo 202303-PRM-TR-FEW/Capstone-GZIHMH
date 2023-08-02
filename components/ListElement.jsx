c7d6948efdaa49df4454b2d93e779631a6bf6586
Izdiharomer <127107528+Izdiharomer@users.noreply.github.com>
2023-08-02 16:44:49 +0300

    231 create no saved courses message component (#232)
    * Feat: no-saved-courses-message-component added
    
    * style: focus property added to the saved and courses page
    
    * Feat: svg added to the no-saved-courses-message-component

--- components/ListElement.jsx
+++ components/ListElement.jsx
@@ -7,8 +7,8 @@ import icons from '../utils/icons';
 import { useDispatch, useSelector } from 'react-redux';
 import { saveCourse, removeCourse } from '@/redux/actions';
 import { selectSavedCourses } from '@/redux/selectors';
-function ListElement() {
 
+function ListElement() {
   const dispatch = useDispatch();
   const savedCourses = useSelector(selectSavedCourses);
   const [selectedCourseIndex, setSelectedCourseIndex] = useState(0);
@@ -25,7 +25,6 @@ function ListElement() {
   const isCourseSaved = (courseExists) => {
     return courseExists ? 'text-primary' : 'text-gray-400';
   };
-  
 
   const handleCourseClick = (index) => {
     setSelectedCourseIndex(index);
@@ -39,22 +38,19 @@ function ListElement() {
   const handleToggle = (courseId) => {
     const course = db.courses.find((course) => course.id === courseId);
     if (course) {
-      //if the course is saved check if the value exist in the store if it is then remove the value//
-      if (course.saved == true) {
+      if (course.saved) {
         const courseExists = savedCourses.some((course) => course.id === courseId);
         if (courseExists) {
           dispatch(removeCourse(courseId));
         }
         course.saved = false;
       } else {
-        //if the course wasn't saved then use the action save course to add it to the store//
         course.saved = true;
         dispatch(saveCourse(course));
-      };
+      }
     }
-    console.log(savedCourses)
-    setRandomCourses(db.courses);
-  }
+    setRandomCourses([...db.courses]);
+  };
 
   return (
     <div className='flex flex-col lg:flex-row md:flex-col sm:flex-col '>
@@ -64,34 +60,46 @@ function ListElement() {
             <h1 className='x-3 py-2'>My Learning</h1>
             <div className='flex flex-row justify-between px-3 py-2 text-gray-400 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-primary hover:text-gray-700'>
               <a className="flex text-lg items-center" href="/pages/statistics">
-                {icons.map(icon => icon.id === 1 && (<div key={icon.id} dangerouslySetInnerHTML={{ __html: icon.svg }} />))}
+                {icons.map((icon) => icon.id === 1 && (<div key={icon.id} dangerouslySetInnerHTML={{ __html: icon.svg }} />))}
                 <span className='px-3 py-2'>Statistics</span>
               </a>
             </div>
           </div>
 
-          <div className=' flex-1 w-full lg:max-h-[800px] overflow-y-auto'>
-
-            {randomCourses.map((course) => (
-              <div className='flex w-full rounded-2xl opacity-100 bg-white hover:bg-blue-200 bg-opacity-30 border hover:border-primary' key={course.id} onClick={() => handleCourseClick(course.id)}>
+          <div className='flex-1 w-full lg:max-h-[800px] overflow-y-auto'>
+            {randomCourses.map((course, index) => (
+              <div
+                className={`flex rounded-2xl opacity-100 bg-white ${
+                  selectedCourseIndex === index ? 'border-primary bg-blue-200' : 'hover:bg-blue-200'
+                } bg-opacity-30 rounded-5 border hover:border-primary`}
+                key={course.id}
+                onClick={() => handleCourseClick(index)}
+              >
                 <div className='flex flex-row gap-2 justify-between'>
-                  <Image src={course.image} width={180} height={152} alt={course.title} className='max-w-[180px] max-h-[152px]' style={{ cursor: 'pointer' }} />
+                  <Image
+                    src={course.image}
+                    width={180}
+                    height={152}
+                    alt={course.title}
+                    className='max-w-[180px] max-h-[152px]'
+                    style={{ cursor: 'pointer' }}
+                  />
                 </div>
-                <div className=' w-full'>
+                <div className='w-full'>
                   <div className='flex justify-between'>
                     <h2>{course.title}</h2>
                     <button onClick={() => handleToggle(course.id)}>
                       <span>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-5 h-5 self-start ${isCourseSaved(savedCourses.some((c) => c.id === course.id))} fill-current m-2`}><path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" /></svg>
-                      </span></button>
+                      </span>
+                    </button>
                   </div>
                   <p className='tutor_name mb-3'>{getPersonName(course.user_id)}</p>
                   <div className="bg-gray-200 h-2 w-auto rounded-xl overflow-hidden mx-2 my-1.5">
-                  <div className="h-full bg-primary rounded-lg" style={{ width: `${parseInt(course.completion_ratio)}%` }}></div>
+                    <div className="h-full bg-primary rounded-lg" style={{ width: `${parseInt(course.completion_ratio)}%` }}></div>
+                  </div>
+                  <p className='text'>{course.completion_ratio} complete</p>
                 </div>
-                  <p className='text'> {course.completion_ratio} complete</p>
-                </div>
-
               </div>
             ))}
           </div>
---
+++ components/NoCourseMessage.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function NoCourseMessage() {
+    return (
+        <div className='absolute top-1/4 left-1/4  items-center justify-center text-center h-full  w-full max-w-[600px] max-h-[500px] '>
+            <div className='bg-white pt-20 rounded-2xl justify-center text-gray-900 font-bold shadow-lg h-full  w-full '>
+                <div className='text-base sm:text-base md:text-base lg:text-base xl:text-base 2xl:text-base @screen (max-width: 300px) text-xs'>
+                    <span>Oops!</span> 
+                    <br/>
+                 You do not have any saved any courses yet
+                 
+                 <div className="flex items-center justify-center h-full pt-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-mood-sad-2" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#9e9e9e" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                      <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+                      <path d="M14.5 16.05a3.5 3.5 0 0 0 -5 0" />
+                      <path d="M10 9.25c-.5 1 -2.5 1 -3 0" />
+                      <path d="M17 9.25c-.5 1 -2.5 1 -3 0" />
+                    </svg>
+                </div>
+                </div>
+                <div className='flex sm:text-xs md:text-sm lg:text-3xl text-3xl flex-col items-center justify-center mt-10 lg:mt-10 md:mt-3 sm:mt-1 gap-2'>
+                  <a className="flex text-lg items-center" href="/pages/home"><button className="text-primary font-bold bg-transparent border border-primary p-2 max-w-[450px] w-full  text-sm h-9  rounded-2xl hover:text-white hover:bg-primary"> Check our featured courses </button> </a>
+                  <a className="flex text-lg items-center" href="/pages/search"><button className="text-primary font-bold bg-transparent border border-primary p-2 max-w-[450px] w-full  h-9 text-sm  rounded-2xl hover:text-white hover:bg-primary">Search your favorite courses</button> </a>
+                </div>
+                
+            </div>
+        </div>
+    );
+}
+
+export default NoCourseMessage;
