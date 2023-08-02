eb2e237ce43bec3d56331ca2988aac14f4eba5ca
hayderali1 <haideralmamori94@gmail.com>
2023-08-02 12:10:29 +0300

    Fix:thisweek component is fixed with props

--- components/ThisWeek.jsx
+++ components/ThisWeek.jsx
@@ -1,69 +1,22 @@
 import React from 'react';
 
-const ThisWeek = () => {
+const ThisWeek = ({ progressData }) => {
   return (
-
-    <div className="flex flex-row  justify-between">
-
-
-    <div className="day-container flex flex-col items-center   m-7 ">
-      <div className="progress-bar bg-gray-200   w-4   h-48   rounded-xl   flex flex-col justify-end">
-        <div className="progress-indicator bg-blue-500   rounded-xl   h-2/3"></div>
-      </div>
-      <div className="font-bold text-black mt-2">Mon</div>
+    <div className="flex flex-row justify-between">
+      {progressData.map((progress, index) => (
+        <div className="day-container flex flex-col items-center m-7" key={index}>
+          <div className="progress-bar bg-gray-200 w-4 h-48 rounded-xl flex flex-col justify-end">
+            <div
+              className="progress-indicator bg-blue-500 rounded-xl"
+              style={{ height: `${progress * 100}%` }}
+            ></div>
+          </div>
+          <div className="font-bold text-black mt-2">
+            {['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'][index]}
+          </div>
+        </div>
+      ))}
     </div>
-
-
-    <div className="day-container flex flex-col items-center   m-7">
-      <div className="progress-bar bg-gray-200   w-4 h-48    rounded-xl  flex flex-col justify-end">
-        <div className="progress-indicator bg-blue-500  rounded-xl  h-3/5"></div>
-      </div>
-      <div className="font-bold text-black mt-2">Tue</div>
-    </div>
-
-
-    <div className="day-container flex flex-col items-center   m-7">
-      <div className="progress-bar bg-gray-200   w-4   h-48   rounded-xl  flex flex-col justify-end">
-        <div className="progress-indicator bg-blue-500   rounded-xl   h-4/5"></div>
-      </div>
-      <div className="font-bold text-black mt-2">Wed</div>
-    </div>
-
-
-    <div className="day-container flex flex-col items-center   m-7">
-      <div className="progress-bar bg-gray-200   w-4   h-48   rounded-xl   flex flex-col justify-end">
-        <div className="progress-indicator bg-blue-500   rounded-xl   h-2/4"></div>
-      </div>
-      <div className="font-bold text-black mt-2">Thu</div>
-    </div>
-
-
-    <div className="day-container flex flex-col items-center   m-7">
-      <div className="progress-bar bg-gray-200   w-4   h-48 rounded-xl flex flex-col justify-end">
-        <div className="progress-indicator bg-blue-500   rounded-xl   h-1/3"></div>
-      </div>
-      <div className="font-bold text-black mt-2">Fri</div>
-    </div>
-
-
-    <div className="day-container flex flex-col items-center   m-7">
-      <div className="progress-bar bg-gray-200   w-4   h-48   rounded-xl   flex flex-col justify-end">
-        <div className="progress-indicator bg-blue-500   rounded-xl   h-5/6"></div>
-      </div>
-      <div className="font-bold text-black mt-2">Sat</div>
-    </div>
-
-
-    <div className="day-container flex flex-col items-center   m-7">
-      <div className="progress-bar bg-gray-200    w-4   h-48   rounded-xl   flex flex-col justify-end">
-        <div className="progress-indicator bg-blue-500   rounded-xl   h-1/2"></div>
-      </div>
-      <div className="font-bold text-black mt-2">Sun</div>
-    </div>
-  </div>
-
-  
-
   );
 };
 
--- components/ThisWeekContainer.jsx
+++ components/ThisWeekContainer.jsx
@@ -15,8 +15,7 @@ const ThisWeekContainer = () => {
     <div className="w-full max-w-4xl p-1 bg-white border border-gray-200 rounded-3xl shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
        <div className=" text-md  text-gray-400 mb-1">Time Spent</div>
        <div className="font-semibold text-black mb-4 mt-1">31.8 hours</div>
-         <ThisWeek />
-
+       <ThisWeek />
 
     </div>
 </div>
