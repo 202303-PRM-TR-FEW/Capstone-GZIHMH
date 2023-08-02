a74aee26e23f80d625a7a5ceb23622559750f02f
hayderali1 <haideralmamori94@gmail.com>
2023-08-02 12:15:21 +0300

    Feat:ThisWeek is added to ThisWeekContainer

--- components/ThisWeekContainer.jsx
+++ components/ThisWeekContainer.jsx
@@ -1,21 +1,22 @@
+
 import React from 'react'
 import ThisWeek from './ThisWeek'
 
 const ThisWeekContainer = () => {
+  const progressData = [0.67, 0.5, 0.8, 0.4, 0.33, 0.83, 0.5];
+
   return (
 
     <div>
-
 
     <div>
        <div className="font-bold text-3xl text-black mb-2"> This Week </div>
     </div>
 
-
     <div className="w-full max-w-4xl p-1 bg-white border border-gray-200 rounded-3xl shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
        <div className=" text-md  text-gray-400 mb-1">Time Spent</div>
        <div className="font-semibold text-black mb-4 mt-1">31.8 hours</div>
-       <ThisWeek />
+       <ThisWeek progressData={progressData} />
 
     </div>
 </div>
