b4998be2c33fc7991c9b30fa01ce1279c1cd4507
Izdihar <ezdiharomerrrr@gmail.com>
2023-08-02 21:12:46 +0300

    style: style of no-saved-courses-message-component added

--- components/NoCourseMessage.jsx
+++ components/NoCourseMessage.jsx
@@ -3,19 +3,19 @@ import { Sad_Face } from '../utils/icons';
 
 function NoCourseMessage() {
     return (
-        <div className='absolute top-1/4 left-1/4  items-center justify-center text-center h-full  w-full max-w-[600px] max-h-[500px] '>
-            <div className='bg-white pt-20 rounded-2xl justify-center text-gray-900 font-bold shadow-lg h-full  w-full '>
-                <div className='text-base sm:text-base md:text-base lg:text-base xl:text-base 2xl:text-base @screen (max-width: 300px) text-xs'>
+        <div className='absolute top-1/4 left-1/4 rounded-2xl shadow-lg bg-white justify-center text-center h-1/2   w-1/2  '>
+            <div className='justify-center text-gray-900 font-bold absolute top-1/4 left-1/4 right-1/4 bottom-1/4 '>
+                <div className=''>
                     <span>Oops!</span> 
                     <br/>
                  You do not have any saved courses yet
-                 <div className="flex items-center justify-center h-full pt-4">
+                 <div className="flex items-center justify-center h-full pb-4">
                     <Sad_Face/>
                 </div>
                 </div>
-                <div className='flex sm:text-xs md:text-sm lg:text-3xl text-3xl flex-col items-center justify-center mt-10 lg:mt-10 md:mt-3 sm:mt-1 gap-2'>
-                  <a className="flex text-lg items-center" href="/pages/home"><button className="text-primary font-bold bg-transparent border border-primary p-2 max-w-[450px] w-full  text-sm h-9  rounded-2xl hover:text-white hover:bg-primary"> Check our featured courses </button> </a>
-                  <a className="flex text-lg items-center" href="/pages/search"><button className="text-primary font-bold bg-transparent border border-primary p-2 max-w-[450px] w-full  h-9 text-sm  rounded-2xl hover:text-white hover:bg-primary">Search your favorite courses</button> </a>
+                <div className='flex flex-col items-center justify-center gap-2 w-full'>
+                  <a className="flex text-sm items-center text-primary font-bold min-w-[170px] bg-transparent border border-primary p-2  h-9 rounded-2xl hover:text-white hover:bg-primary" href="/pages/home"> Check our featured courses  </a>
+                  <a className="flex text-sm items-center text-primary font-bold min-w-[170px] bg-transparent border border-primary p-2  h-9 rounded-2xl hover:text-white hover:bg-primary" href="/pages/search">Search your favorite courses </a>
                 </div>
                 
             </div>
