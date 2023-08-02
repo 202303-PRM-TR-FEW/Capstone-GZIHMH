1773f11d512d6210be1f5023055dc4b933134f72
aaron176 <haroon.seena2017@gmail.com>
2023-08-02 12:36:51 +0300

    Course Deatil page uncompleted

---
+++ app/pages/CourseDetails/page.jsx
@@ -0,0 +1,38 @@
+"use client";
+import React, { useState } from "react";
+import ProgressCourses from "@/components/ProgressCourses";
+import CourseVideo from "@/components/CourseVideo";
+import coursesdb from "@/utils/coursesdb";
+
+import data from "@/utils/firebase";
+
+const CoursesDetails = () => {
+  const [showVideos, setShowVideo] = useState(true);
+
+  console.log(data);
+  const info = coursesdb.initialLessons;
+
+  const [active, setActive] = useState(0);
+
+  const revealVideos = () => {
+    setShowVideo((prevState) => !prevState);
+  };
+
+  const findVideo = (id) => {
+    const video = info.find((video) => video.courseId === id);
+    setActive(video.link);
+  };
+
+  return (
+    <div>
+      <ProgressCourses
+        showVideos={showVideos}
+        revealVideos={revealVideos}
+        info={info}
+      />
+      <CourseVideo active={active} />
+    </div>
+  );
+};
+
+export default CoursesDetails;
---
+++ components/VideoCourse.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Image from "next/image";
+import coursesdb from "@/utils/coursesdb";
+const CourseVideo = ({ info, active }) => {
+  return (
+    <div>
+      <iframe src={active} title={linkTitle} allowFullScreen />
+      <div>
+        <Image
+          src={coursesdb.initialUsers.profilePic}
+          alt="Pic"
+          height={20}
+          width={50}
+        />
+        <p>{coursesdb.initialUsers.name}</p>
+      </div>
+      <div>
+        <div>
+          <p>{info.duration}</p>
+        </div>
+        <div>
+          <p>{`${info.rating}/5.0`}</p>
+        </div>
+        <div>
+          <h1>Course Discription</h1>
+          <p>{coursesdb.initialCourses.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CourseVideo;
