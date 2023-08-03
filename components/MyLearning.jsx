5a338478a4e82157fab18d96e469fadc77d5ac06
Perry <116006513+Gperyy@users.noreply.github.com>
2023-08-03 19:50:42 +0300

    home/fetch my learning || redux myLearning (#239)

--- components/MyLearning.jsx
+++ components/MyLearning.jsx
@@ -1,63 +1,98 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
-import BecomingPhoto from '../public/assets/images/photo-1541516160071-4bb0c5af65ba.jpeg';
-import DesignThinking from "../public/assets/images/2c1a496b-9948-4cb1-b87e-dbd417261382.jpg";
+import { useDispatch, useSelector } from 'react-redux';
+import { saveCourse, removeCourse } from '@/redux/actions';
+import { selectSavedCourses } from '@/redux/selectors';
+import db from '../utils/db';
+import { shuffleArray } from '@/utils/helpers';
 
 const MyLearning = () => {
+    const dispatch = useDispatch();
+    const savedCourses = useSelector(selectSavedCourses);
+    const [coursesToShow, setCoursesToShow] = useState([]);
+
+    useEffect(() => {
+        const shuffledCourses = shuffleArray(db.courses);
+        setCoursesToShow(shuffledCourses.slice(0, 2));
+    }, []);
+
+    const isCourseSaved = (courseId) => {
+        return savedCourses.some((course) => course.id === courseId);
+    };
+
+    const handleToggle = (courseId) => {
+        const course = db.courses.find((course) => course.id === courseId);
+        if (course) {
+            if (isCourseSaved(course.id)) {
+                dispatch(removeCourse(course.id));
+            } else {
+                const newCourse = { ...course, saved: true };
+                dispatch(saveCourse(newCourse));
+            }
+        }
+    };
+
+    const handleCourseClick = (courseId) => {
+        console.log(`Navigating to course with id: ${courseId}`);
+    };
+
+    const getPersonName = (user_id) => {
+        const person = db.person.find((p) => p.id === user_id);
+        return person ? person.name : '';
+    };
+
     return (
-        <div className="flex flex-col lg:flex-row md:flex-col sm:flex-col h-full">
-            <div className="flex rounded-2xl w-[680px] h-[172px] m-3 opacity-100 bg-white hover:bg-blue-200 bg-opacity-30 rounded-5 border hover:border-primary shadow">
-                <Image
-                    src={BecomingPhoto}
-                    width={190}
-                    height={172}
-                    alt={'Becoming Photo'}
-                    className="flex flex-row"
-                />
-                <div className="ml-4 p-4 w-[510px]">
-                    <div className="flex flex-row justify-between mt-4">
-                        <div className="px-1">
-                            <h2>Becoming a Photographer</h2>
-                            <p className="text-gray-600">Clara Chaning</p>
-                        </div>
+        <div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
+            {coursesToShow.map((course) => (
+                <div
+                    key={course.id}
+                    className='relative flex items-start justify-start p-1 rounded-xl dark:bg-white dark:text-white text-slate-700 cursor-pointer'
+                    onClick={() => handleCourseClick(course.id)}
+                >
+                    <div className='w-[180px] h-[152px] relative rounded-2xl overflow-hidden'>
+                        <Image
+                            src={course.image}
+                            layout='fill'
+                            objectFit='cover'
+                            alt={course.title}
+                        />
                     </div>
-                    <div className="flex items-center justify-between my-2">
-                        <div className="w-full h-2 bg-gray-200 rounded-full">
-                            <div className="w-2/3 h-full text-center text-xs text-white bg-blue-600 rounded-full"></div>
+                    <div className='flex flex-col flex-grow p-3'>
+                        <div className='flex justify-between'>
+                            <h2 className='text-xl font-bold mr-8'>{course.title}</h2>
+                            <button
+                                onClick={() => handleToggle(course.id)}
+                                className={`absolute p-2 rounded-lg text-slate-400 group top-4 right-4 bg-slate-300 hover:cursor-pointer hover:bg-blue-200 group ${
+                                    isCourseSaved(course.id) ? 'bg-blue-400' : 'bg-gray-400'
+                                }`}
+                            >
+                                <span>
+                                    <svg
+                                        stroke="currentColor"
+                                        fill="currentColor"
+                                        strokeWidth="0"
+                                        viewBox="0 0 16 16"
+                                        className="group-hover:text-blue-600 "
+                                        height="16"
+                                        width="16"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z"></path>
+                                    </svg>
+                                </span>
+                            </button>
                         </div>
-                    </div>
-                    <p className="text-black text-sm">
-                        69% Completed
-                    </p>
-                </div>
-            </div>
-            <div className="flex flex-col lg:flex-row md:flex-col sm:flex-col h-full">
-                <div className="flex rounded-2xl w-[680px] h-[172px] m-3 opacity-100 bg-white hover:bg-blue-200 bg-opacity-30 rounded-5 border hover:border-primary shadow">
-                    <Image
-                        src={DesignThinking}
-                        width={190}
-                        height={172}
-                        alt={'Becoming Photo'}
-                        className="flex flex-row"
-                    />
-                    <div className="ml-4 p-4 w-[510px]">
-                        <div className="flex flex-row justify-between mt-4">
-                            <div className="px-1">
-                                <h2>Design Thinking 2.0</h2>
-                                <p className="text-gray-600">Chris Kinley</p>
-                            </div>
+                        <p className='tutor_name mb-3'>{getPersonName(course.user_id)}</p>
+                        <div className='bg-gray-200 h-2 w-full rounded-xl overflow-hidden mx-2 my-1.5'>
+                            <div
+                                className='h-full bg-primary rounded-lg'
+                                style={{ width: `${parseInt(course.completion_ratio)}%` }}
+                            ></div>
                         </div>
-                        <div className="flex items-center justify-between my-2">
-                            <div className="w-full h-2 bg-gray-200 rounded-full">
-                                <div className="w-2/3 h-full text-center text-xs text-white bg-blue-600 rounded-full"></div>
-                            </div>
-                        </div>
-                        <p className="text-black text-sm">
-                            27% Completed
-                        </p>
+                        <p className='text'>{course.completion_ratio} complete</p>
                     </div>
                 </div>
-            </div>
+            ))}
         </div>
     );
 };
