9affb902a8c87e68de0e79714fec515c3c731151
mohammedhayaly <mohammed.alhayali88@yahoo.com>
2023-08-04 16:56:40 +0300

    goal and streak fixed

--- app/pages/statistics/page.jsx
+++ app/pages/statistics/page.jsx
@@ -39,6 +39,21 @@ const CheckIcon3 = `
 
 
 
+const Logo1 = `
+<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-dot" width="20" height="20" viewBox="0 0 24 24" stroke-width="3" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+              <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+              <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+            </svg>`;
+
+
+const Logo2 = `
+<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-star" width="20" height="20" viewBox="0 0 24 24" stroke-width="3" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+<path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
+</svg>`;
+
+
 
 const page = () => {
   return (
@@ -147,8 +162,30 @@ const page = () => {
 
 
 
-  <div className="m-1">   {/* box 2 */}
-  <StatisticsGoalStreak />
+  <div className="m-1 flex flex-row">   {/* box 2 */}
+
+
+  <StatisticsGoalStreak
+  
+  title ="GOAL"
+  dyas="2/3 dyas"
+  imageSrc={Logo1}
+  background="bg-white" 
+  box="bg-blue-50"
+
+  />
+
+<StatisticsGoalStreak
+  
+  title ="STREAK"
+  dyas="103 dyas"
+  textcolor="text-lime-500"
+  imageSrc={Logo2}
+  background="bg-white" 
+  box="bg-lime-50"
+
+  />
+
   
   </div>
 
