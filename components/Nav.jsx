dd7cd449a31f8a231e60651d55859be2270b292c
Zeyna Baaj <zainabaaj@gmail.com>
2023-08-04 22:44:30 +0300

    style:navbar and icons

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import { signOut } from 'firebase/auth'
 import { auth,getDoc,doc,firestore} from'@/utils/firebase'
 import { getProviders } from 'next-auth/react'
-import { HomeIcon, Logout, ProfileIcon, SavedIcon, SearchIcon}from '@/utils/icons'
+import { HomeIcon, Logout, ProfileIcon, SavedIcon, CoursesIcon, SearchIcon}from '@/utils/icons'
 const Nav = ({ router }) => {
     const {uid} = auth.currentUser
     
@@ -35,7 +35,7 @@ const Nav = ({ router }) => {
     // console.log("resule of isAnon is ",isAnon())
     return (
         <div>
-            <aside className="flex flex-col w-20 h-screen px-5 py-8 overflow-y-auto bg-transparent dark:bg-gray-900 dark:border-gray-700">
+            <aside className="flex flex-col w-20  px-5 py-8 overflow-y-auto bg-transparent ">
                 <a href="/" >
                     <Image src="/assets/icons/logos.png"
                         alt="icon image"
@@ -50,36 +50,36 @@ const Nav = ({ router }) => {
                     <nav className="-mx-3 space-y-6 ">
                         <div className="space-y-3 ">
 
-                            <a className=" flex flex-col items-center px-3 py-2 text-gray-400 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-primary hover:text-primary" href="/pages/home">
+                            <a className=" flex flex-col items-center px-3 py-2 text-gray-400 transition-colors duration-300 transform rounded-lg  hover:bg-gray-100   hover:text-primary" href="/pages/home">
                                <HomeIcon/>
                                 <span className="mx-2 text-sm font-medium">Home</span>
                             </a>
 
-                            <a className="flex flex-col items-center px-3 py-2 text-gray-400 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-primary hover:text-primary" href="/pages/search">
+                            <a className="flex flex-col items-center px-3 py-2 text-gray-400 transition-colors duration-300 transform rounded-lg  hover:bg-gray-100   hover:text-primary" href="/pages/search">
                                 <SearchIcon/>
 
                                 <span className="mx-2 text-sm font-medium">Search</span>
                             </a>
-                            <a className="flex flex-col items-center px-3 py-2 text-gray-400 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-primary hover:text-primary" href="/pages/courses">
-                                <coursesIcon />
+                            <a className="flex flex-col items-center px-3 py-2 text-gray-400 transition-colors duration-300 transform rounded-lg  hover:bg-gray-100   hover:text-primary" href="/pages/courses">
+                                <CoursesIcon />
 
                                 <span className="mx-2 text-sm font-medium">Courses</span>
                             </a>
-                            <a className="flex flex-col items-center px-3 py-2 text-gray-400 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-primary hover:text-primary" href="/pages/saved">
+                            <a className="flex flex-col items-center px-3 py-2 text-gray-400 transition-colors duration-300 transform rounded-lg  hover:bg-gray-100   hover:text-primary" href="/pages/saved">
                                 <SavedIcon/>
 
                                 <span className="mx-2 text-sm font-medium">Saved</span>
                             </a>
-                            <a className="flex flex-col items-center px-3 py-2 text-gray-400 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-primary hover:text-primary" href="/pages/profile">
+                            <a className="flex flex-col items-center px-3 py-2 text-gray-400 transition-colors duration-300 transform rounded-lg  hover:bg-gray-100   hover:text-primary" href="/pages/profile">
                                <ProfileIcon/>
 
                                 <span className="mx-2 text-sm font-medium">Profile</span>
                             </a>
                             {
                                 !process.env.ISANON? (
-                                    <div className='flex flex-col items-center px-3 py-2 text-gray-400 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-primary hover:text-primary'>
+                                    <div className='flex flex-col items-center px-3 py-2 justify-center m-auto text-gray-400 transition-colors duration-300 transform rounded-lg  hover:bg-gray-100  hover:text-primary'>
                                         
-                                        <button onClick={handleSignoutClick}>
+                                        <button className="flex flex-col text-sm font-medium items-center justify-center m-auto text-gray-400 hover:bg-gray-100  hover:text-primary" onClick={handleSignoutClick}>
                                         <Logout/>
                                 sign out
                             </button>
