262b7f7364858041d49060aaf7601fd2575fd675
zeyna baaj <zainabaaj@gmail.com>
2023-08-09 18:46:53 +0300

    Update getCategories.js

--- app/pages/api/getCategories.js
+++ app/pages/api/getCategories.js
@@ -4,11 +4,9 @@ import {firestore, collection, getDocs, docs} from ‘@/utils/firebase’
 
 
 
-Const snapshot =getDocs(categories)
-Const catdata = snapshot.docs.map((doc) => doc.data() )
 
 Const getcategories = async ()=> {
-    Const categories = collection(firestore,’caregories’)
+    Const categories = collection(firestore,'caregories')
     Const snapshot = await getDocs(categories)
     Const catdata = snapshot.docs.map((doc) => doc.data() )
     return catdata
